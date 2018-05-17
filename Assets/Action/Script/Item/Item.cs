24cd330579412612574a524b071b1b43b0624689
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-05-17 19:06:39 +0900

    屋台編集

--- Assets/Action/Script/Item/Item.cs
+++ Assets/Action/Script/Item/Item.cs
@@ -18,14 +18,15 @@ public abstract class Item : MonoBehaviour
     {
     }
 
-    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    protected virtual void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collision.gameObject.tag != "Player") return;
+        Debug.Log("entered");
+        if (collider.gameObject.tag != "Player") return;
 
         /*Player player = collision.gameObject.GetComponent<Player>();
         if (colorId != -1 && colorId != player.id) return;*/
 
-        EffectFire(collision.gameObject.GetComponent<PlayerStatus>());
+        EffectFire(collider.gameObject.GetComponent<PlayerStatus>());
 
         GetComponent<Collider2D>().enabled = false;
         Destroy(gameObject);
