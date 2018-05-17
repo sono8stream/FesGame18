721bdac797781c8e1eca6bb039910954fdcd3a3b
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-05-18 00:24:56 +0900

    ステージ画像追加 タイトル等微修正

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour {
+public class CameraMove : MonoBehaviour
+{
 
     new Vector3 pos;
 
-	// Use this for initialization
-	void Start () {
-       pos = transform.position;
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         pos.x = pos.x - 0.5f;
         transform.position = pos;
-	}
+    }
 }
