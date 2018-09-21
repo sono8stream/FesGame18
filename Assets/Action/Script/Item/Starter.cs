dfe2e91a922ade586fb2d03aced29375e2573309
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-09-21 20:26:19 +0900

    チュートリアル実装

--- Assets/Action/Script/Item/Starter.cs
+++ Assets/Action/Script/Item/Starter.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Starter : MonoBehaviour {
+public class Starter : MonoBehaviour
+{
 
     [SerializeField]
     MonoBehaviour targetScript;
