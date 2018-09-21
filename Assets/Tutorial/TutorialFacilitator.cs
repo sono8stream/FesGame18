a7ded4b9d136cbf940e4183c5275b17b83ac837c
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-09-22 08:01:26 +0900

    チュートリアルの制御プログラムを追加

---
+++ Assets/Tutorial/TutorialFacilitator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TutorialFacilitator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] players;
+
+    int playerCnt;
+    int stateNo;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerCnt = players.Count(x => x.activeSelf);
+        stateNo = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (stateNo)
+        {
+            case 0:
+                break;
+        }
+    }
+}
