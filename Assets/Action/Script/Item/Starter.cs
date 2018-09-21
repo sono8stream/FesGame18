06f0bc6a74d11edf95755dd5f7fedc08b7c1586b
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-09-21 10:25:06 +0900

    時間差で生成されるアイテムを制御するスクリプトを生成

---
+++ Assets/Action/Script/Item/Starter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starter : MonoBehaviour {
+
+    [SerializeField]
+    MonoBehaviour targetScript;
+    [SerializeField]
+    float startSec;
+    [SerializeField]
+    bool enableOrNot = true;
+
+    TimeCounter timeCounter;
+    bool onEnd;
+
+    // Use this for initialization
+    void Awake()
+    {
+        TimeCounter timeCounter = new TimeCounter(startSec);
+        timeCounter.Start();
+        targetScript.enabled = !enableOrNot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onEnd) return;
+        //Debug.Log(timeCounter);
+        if (timeCounter == null)
+        {
+            timeCounter = new TimeCounter(startSec);
+            timeCounter.Start();
+            //Debug.Log("ReStart");
+        }
+        if (timeCounter.OnLimit())
+        {
+            targetScript.enabled = enableOrNot;
+            onEnd = true;
+        }
+    }
+}
---
+++ Assets/CommonScript/Common/ScriptSwitcher.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    TimeSpan[] timeSpans;
+    [SerializeField]
+    MonoBehaviour[] targetScripts;
+
+    TimeCounter timeCounter;
+    Counter spanCounter;
+    bool on;
+
+    private void Start()
+    {
+        Array.Sort(timeSpans);
+        timeCounter = new TimeCounter(timeSpans[0].beginSec);
+        spanCounter = new Counter(timeSpans.Length);
+        on = false;
+        SwitchScripts();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeCounter.OnLimit())
+        {
+            on = !on;
+            SwitchScripts();
+            if (on)
+            {
+                
+            }
+            else
+            {
+                if (spanCounter.Count())
+                {
+                    this.enabled = false;
+                }
+            }
+        }
+    }
+
+    void SwitchScripts()
+    {
+        for (int i = 0; i < targetScripts.Length; i++)
+        {
+            targetScripts[i].enabled = on;
+        }
+    }
+}
+
+[Serializable]
+public class TimeSpan :IComparable<TimeSpan>
+{
+    public float beginSec { get; }
+    public float endSec { get; }
+
+    public TimeSpan(float begin,float end)
+    {
+        beginSec = begin;
+        endSec = end;
+    }
+
+    public int CompareTo(TimeSpan otherSpan)
+    {
+        if (beginSec < otherSpan.beginSec)
+        {
+            return -1;
+        }
+        else if (beginSec == otherSpan.beginSec)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
