141d904eeb44a56a6e3e91203d70296efc7d05fa
小井田大賀 <tk_kakutasu@yahoo.co.jp>
2018-09-22 05:58:00 +0900

    チュートリアル製作(途中)

---
+++ Assets/Action/Script/TutorialManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour {
+
+    public StandManager standManager;
+    private int tutorialLevel = 0;
+    public Text tutorialText;
+    public GameObject[] tutorialObjects;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch(tutorialLevel){
+            //屋台を立てる
+            case 0:
+                if(standManager.isStand){
+                    tutorialLevel++;
+                }
+                break;
+                //お金回収
+            case 1:
+
+                break;
+                //爆弾をもつ
+            case 2:
+                break;
+                //屋台を壊す
+            case 3:
+                break;
+            case 4:
+
+                break;
+            default:
+                break;
+        }
+	}
+
+}
